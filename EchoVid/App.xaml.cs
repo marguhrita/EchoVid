cbe2e8109e585dc17f24a67bdfcb4c32eba7c730
Alastair <s2157894@ed.ac.uk>
2025-01-11 17:31:48 +0000

    MAUI init

---
+++ EchoVid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EchoVid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EchoVid/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EchoVid
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
